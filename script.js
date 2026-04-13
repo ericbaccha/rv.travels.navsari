f90fe214655ccea8fde7021ddf4f2891292d5b88
ericbaccha <hheehaa831@gmail.com>
2026-04-13 08:22:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// MOBILE MENU
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+if(btn){
+  btn.addEventListener("click", ()=>{
+    menu.classList.toggle("show");
+  });
+}
+
+// FAST FADE
 const faders = document.querySelectorAll(".fade");
 
-window.addEventListener("scroll", () => {
-  faders.forEach(el => {
-    if(el.getBoundingClientRect().top < window.innerHeight - 80){
+window.addEventListener("scroll", ()=>{
+  faders.forEach(el=>{
+    if(el.getBoundingClientRect().top < window.innerHeight - 40){
       el.classList.add("show");
     }
   });
