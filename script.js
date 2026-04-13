f05fadbc1fa8f183bb4fbb970920c34efed58d0f
ericbaccha <hheehaa831@gmail.com>
2026-04-13 23:57:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+// MENU
 const btn = document.getElementById("menu-btn");
 const mobile = document.getElementById("mobile-menu");
 
 if(btn && mobile){
-btn.addEventListener("click", function(){
+btn.addEventListener("click", () => {
 mobile.classList.toggle("active");
 });
 }
 
-// smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
-anchor.addEventListener('click', function(e){
-e.preventDefault();
-const target = document.querySelector(this.getAttribute("href"));
-if(target){
-target.scrollIntoView({behavior:"smooth"});
+// ANIMATION
+const elements = document.querySelectorAll(".animate");
+
+function reveal(){
+elements.forEach(el => {
+const top = el.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+el.classList.add("show");
 }
 });
-});
+}
+
+// RUN ALWAYS
+reveal();
+window.addEventListener("scroll", reveal);
 
 });
